e279b138caa5194988cd5e210c2cf9e89f0cb5db
Bruno-Martin Schuster <bruno.m.schuster@gmail.com>
2023-01-24 17:54:49 +0100

    Add basic  Routes and file structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 function App() {
   return (
-<React.Fragment>
-  <div className='App'></div>
-</React.Fragment>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
-
-
---
+++ src/user/components/UserItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const UsersItem = (props) => {
+return <React.Fragment>
+
+</React.Fragment>
+};
+
+export default UsersItem;
